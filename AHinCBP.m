94e07623f33260ad036df989486129060f7ab242
AlinaPanzel <alina.panzel@web.de>
2025-09-11 18:20:06 +0200

    Update AHinCBP function

--- AHinCBP.m
+++ AHinCBP.m
@@ -2,7 +2,7 @@
 
 
 % Author: Alina Panzel
-% Last Date of Changes: 10.09.2025
+% Last Date of Changes: 11.09.2025
 
 %% Load Data & Atlases
 
@@ -22,12 +22,11 @@ a = struct; % for atlases
 % Get metadata table
 d = get_metadata(d);
 
-% Sorts, loads and gets longitudinal fMRI and behavioral metadata 
-[d, lo] = get_all_longitudinal(d, lo, datadir);
+% Load in behvaioral & fmri data
+[d, lo] = load_data_objects(d, datadir, lo);
 
-% Load Atlases
+% Load atlases
 a = load_MSS_atlases(a);
-
 
 %% Behavioral Analysis
 
@@ -36,39 +35,26 @@ a = load_MSS_atlases(a);
 
 %% Neural Analysis
 
-
-% Load ROIs of comparison interest
-areas = {'A1','MGN','IC','S1_L','S1_R', 'm_ventral_insula', 'm_dorsal_insula', 'm_posterior_insula', 'mPFC', 'precuneus', 'PCC'};
-sessions = {'S1','S2'};
-Treatment = {'G1' 'G2' 'G3'};
-field =  {'all_s_l', 'all_s_h', 'completers_s_l', 'completers_s_h'}; %'s_l' 's_h', 
-for n = 1:numel(Treatment)
-    for s = 1:numel(sessions)
-        for u = 1:numel(areas)
-            for i = 1:numel(field)  %numel(fields) % number of conditions
-                r = extract_roi_averages(lo.(Treatment{n}).(sessions{s}).(field{i}), a.(areas{u})); 
-                lo.roi.(Treatment{n}).(sessions{s}).(areas{u}).(field{i}) = cat(2, r.dat); 
-            end
-        end
-    end
-end
-
-% Load MVPA patterns of comparison interest
-for g = 1:numel(Treatment)
-    for s = 1:numel(sessions)
-        for i = 1:numel(field)
-            pat_tabl.(Treatment{g}).(sessions{s}).(field{i}) = apply_FM_patterns(lo.(Treatment{g}).(sessions{s}).(field{i}), 'cosine_similarity'); % Fibromyalgia patterns
-            prexps.(Treatment{g}).(sessions{s}).(field{i}) = apply_multiaversive_mpa2_patterns(lo.(Treatment{g}).(sessions{s}).(field{i})); % Negative affect patterns
-        end
-    end 
-end
+% Load in ROI and MVPA values
+lo = load_ROI(a, lo);
+lo = load_MVPA(lo);
 
 
 
-% Baseline Analysis 
+
+%% Plots & Visualizations
+
+% Get Baseline Figures
+
+% Get longitudinal figure
 
 
-% Longitudinal Analysis 
+% -------- Baseline Analysis ----------
+
+update = struct;
+update1 = update; 
+
+% ------- Longitudinal Analysis -------
 
 % Get data tables in long format 
 con_table_long = get_longformat_table(d,lo, pat_tabl, prexps);
@@ -77,9 +63,3 @@ con_table_long = get_longformat_table(d,lo, pat_tabl, prexps);
 
 
 
-
-
-
-
-% Get sorted brain measures -> only wors
-%[con_table_lo, con_table_hi] = get_data_tables(d, pat_tabl, prexps)
